c7689a6b6c472bf395861597995de9e41e49ee41
Himaja Marni <himarni@outlook.com>
2018-05-29 19:07:36 -0700

    corrected cosmetic errors

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => (
 <div className="Main">
 <h3>Welcome Admin</h3>
 <h3 className="UserEntry"><Link to = "/customerForm">User Entry</Link></h3>
-<h4>Click add some user details</h4>
+<h5>Click to add user details</h5>
 </div>
 
 );
